040eed33391d48c6be9fb9efd017cedb9438c5a0
Firoz shaik <shaikfirozahmad99@gmail.com>
2024-10-05 21:27:32 +0530

    updated validateBillEditReq

--- src/services/store_service.js
+++ src/services/store_service.js
@@ -2083,13 +2083,8 @@ class StoreService {
                 throw new Error('Bill associated with the request not found');
             }
 
-            console.log(isApproved)
-            console.log(typeof isApproved)
-            console.log(!isApproved)
-            console.log(isApproved == false)
-
             // Handle rejection first
-            if (isApproved == false) {
+            if (isApproved == "false") {
                 billEditReq.isApproved = false;
                 billEditReq.dateOfValidate = new Date();
                 originalBill.editStatus = 'REJECTED';
